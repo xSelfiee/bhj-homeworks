4e75a1b5304fe0902372e00f8404d34d32928a7b
Oleg Smyrnov <smirnovoleg95@gmail.com>
2021-04-01 15:26:41 +0200

    Зад. 6.1

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,15 @@
+const hasTooltip = document.querySelectorAll('.has-tooltip');
+
+hasTooltip[0].insertAdjacentHTML('beforeend', '<div class="tooltip" style="left: 0; top: 0"></div>');
+let tooltip = document.querySelector('.tooltip');
+
+hasTooltip.forEach((text) => {
+    text.addEventListener('click', (event) => {
+        event.preventDefault();
+            
+        tooltip.classList.add('tooltip_active');
+        tooltip.textContent = event.currentTarget.getAttribute('title');;
+        tooltip.style.left = event.currentTarget.offsetLeft + 0 + "px";
+        tooltip.style.top = event.currentTarget.offsetTop + 20 + "px";
+  });
+});
